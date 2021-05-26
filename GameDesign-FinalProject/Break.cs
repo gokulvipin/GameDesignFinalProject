70b4c3288c10bfeb7d09cb7e490c9fc1fcacf57b
gokulvipin <69372227+gokulvipin@users.noreply.github.com>
2021-05-26 11:37:57 -0500

    Update Break.cs

--- GameDesign-FinalProject/Break.cs
+++ GameDesign-FinalProject/Break.cs
@@ -1,29 +1,41 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Break : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     public int maxHits;
     int timesHit;
+    private Vector3 Barrel;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        timesHit = 0; 
+        timesHit = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Barrel = this.transform.parent.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = Barrel;
+        }
     }
-
-    private void OnCollisionEnter2D(Collision2D bullet)
+    
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         timesHit++;
         if (timesHit >= maxHits)
         {
-            Destroy(gameObject);
+            transform.position = new Vector3(0, 180f, 0);
+            timesHit = 0;
         }
     }
+
 }
+
